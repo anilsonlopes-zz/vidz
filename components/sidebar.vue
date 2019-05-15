55e6bfa602d399d8ae79ffcbb5cc20dbd1d93758
Anilson Lopes <nissobmx@gmail.com>
2019-05-15 00:18:19 -0300

    change brand name, version and description

--- components/sidebar.vue
+++ components/sidebar.vue
@@ -2,7 +2,7 @@
   <div class="hidden fixed animated fadeIn slow pin-t pin-l pin-b md:block max-w-xs w-64 bg-grey-lighter text-grey-lighter h-screen overflow-y-auto scroll-custom">
     <div id="brand" class="p-4 text-center">
       <nuxt-link :to="{ name: 'index' }" tag="h2" class="cursor-pointer text-black">
-        AnimesVision
+        {{ pkg.name }} <sub class="text-grey-dark text-sm font-thin"><small>{{ pkg.version }}</small></sub>
       </nuxt-link>
     </div>
     <div id="mediaMenu" class="pt-4 pb-8 my-4 border-b">
@@ -60,8 +60,11 @@
 </template>
 
 <script>
+import pkg from '~/package.json'
+
 export default {
   data: () => ({
+    pkg,
     mediaMenu: [
       {
         name: 'Séries',
